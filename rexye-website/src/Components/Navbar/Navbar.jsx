11449cc87e8a0f3d8945b25af09dea7ba633842a
Rex Ye <rxye1991@gmail.com>
2020-08-21 13:18:02 -0400

    createdNavBarComponent

---
+++ rexye-website/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import './Navbar.css'
+
+class Navbar extends Component {
+
+    render(){
+        return(
+            <div className='navbar-component'>
+                <div className='logo'>
+                        RexYe Website
+                </div>
+                <ul>
+                    <li>About</li>
+                    <li>Resume</li>
+                    <li>Projects</li>
+                    <li>Blogs</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Navbar 
